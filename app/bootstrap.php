a816ad439fc968c4f44448dd7dfd64df5d7fcc03
Pavel Paulík <pavel.paulik@seznam.cz>
2019-10-12 22:44:30 +0200

    updated better bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,34 +2,24 @@
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-// @todo module loader is not complete yet
-$configurator = new \Devrun\Config\Configurator(dirname(__DIR__) . '/app'/*, $loader*/);
+$configurator = new \Devrun\Config\Configurator(dirname(__DIR__) . '/app', $debugMode = null, $loader);
 
-$configurator->setDebugMode($remoteIP = '172.18.0.1'); // enable for your remote IP
-//$configurator->setDebugMode(false);
-//$configurator->enableDebugger();
-
-if (\Devrun\Utils\Debugger::getIPAddress() == '37.221.241.103') {
-    $configurator->setDebugMode(true);
-}
-
-$configurator->enableDebugger();
 error_reporting(~E_USER_DEPRECATED); // note ~ before E_USER_DEPRECATED
-umask(0000);
 
 $robotLoader = $configurator->createRobotLoader();
 $robotLoader
     ->addDirectory(__DIR__)
-//    ->addDirectory(__DIR__ . '/../vendor/devrun') // developer mode only
-    ->ignoreDirs .= ', Qtests, test, resources';
+    ->ignoreDirs .= ', templates, test, resources';
 $robotLoader->register();
 
-$environment = (Nette\Configurator::detectDebugMode(['127.0.0.1', $remoteIP]) or (PHP_SAPI == 'cli' && Nette\Utils\Strings::startsWith(getHostByName(getHostName()), "127.0.")))
+$environment = $configurator->isDebugMode()
     ? 'development'
     : 'production';
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . "/config/config.$environment.neon");
+$configurator->addConfig(__DIR__ . '/sandbox/config/config.neon');
+if (file_exists($environmentConfig = __DIR__ . "/config/config.$environment.neon")) {
+    $configurator->addConfig(__DIR__ . "/config/config.$environment.neon");
+}
 
 if (($agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'admin') == 'admin') {
     $environment = 'admin';
--- app/sandbox.php
+++ app/sandbox.php
@@ -1,17 +1,18 @@
 <?php
 
 return array(
-    'appDir'       => __DIR__,
-    'configDir'    => __DIR__ . '/config',
-    'dataDir'      => __DIR__ . '/data',
-    'modulesDir'   => __DIR__ . '/modules',
-    'baseDir'      => dirname(__DIR__),
-    'logDir'       => dirname(__DIR__) . '/log',
-    'tempDir'      => dirname(__DIR__) . '/temp',
-    'libsDir'      => dirname(__DIR__) . '/vendor', // lze zadat aktuální umístění devrunu
-    'wwwDir'       => dirname(__DIR__) . '/www',
-    'imageDir'     => dirname(__DIR__) . '/images',
-    'wwwCacheDir'  => dirname(__DIR__) . '/webTemp',
-    'publicDir'		=> dirname(__DIR__) . '/media',
-    'resourcesDir' => dirname(__DIR__) . '/resources',
+    'appDir'        => __DIR__,
+    'configDir'     => __DIR__ . '/config',
+    'dataDir'       => __DIR__ . '/data',
+    'modulesDir'    => __DIR__ . '/modules',
+    'baseDir'       => dirname(__DIR__),
+    'logDir'        => dirname(__DIR__) . '/log',
+    'tempDir'       => dirname(__DIR__) . '/temp',
+    'libsDir'       => dirname(__DIR__) . '/vendor', // lze zadat aktuální umístění devrunu
+    'wwwDir'        => dirname(__DIR__) . '/www',
+    'imageDir'      => dirname(__DIR__) . '/images',
+    'wwwCacheDir'   => dirname(__DIR__) . '/webTemp',
+    'publicDir'     => dirname(__DIR__) . '/media',
+    'resourcesDir'  => dirname(__DIR__) . '/resources',
+    'migrationsDir' => dirname(__DIR__) . '/migrations',
 );
