f73ff290278f3dc615b39af2f88748aea45e08b1
Pavel Paulík <pavel.paulik@seznam.cz>
2019-10-17 23:57:34 +0200

    added Utils/Version git example

---
+++ Devrun/Utils/Version.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Devrun\Module;
+
+use DateTime;
+use DateTimeZone;
+use Exception;
+
+class Version
+{
+    const MAJOR = 1;
+    const MINOR = 2;
+    const PATCH = 3;
+
+    /**
+     * @return string
+     * @throws Exception
+     */
+    public static function get()
+    {
+        $commitInfo = trim(exec('git log -1 --pretty=format:\'%h - %s (%ci)\' --abbrev-commit'));
+        dump($commitInfo);
+
+        $commitInfo = trim(exec('git describe --tags'));
+        dump($commitInfo);
+
+        $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+
+        $commitDate = new DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
+        $commitDate->setTimezone(new DateTimeZone('UTC'));
+
+        return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:i:s'));
+    }
+}
