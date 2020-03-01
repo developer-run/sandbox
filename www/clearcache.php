083a7b333dbbef73c959c4ef7adf7df642d9533e
Pavel Paulík <pavel.paulik@seznam.cz>
2020-03-01 07:55:48 +0100

    update migrations dir

--- www/clearcache.php
+++ www/clearcache.php
@@ -2,6 +2,9 @@
 
 opcache_reset();
 require __DIR__ . '/../vendor/autoload.php';
-\Devrun\Utils\FileTrait::purge($dir = dirname(__DIR__) . "/temp/cache");
 
+$dir = dirname(__DIR__) . "/temp/cache";
+echo "<span style='color: greenyellow; font-weight: bold;'>'$dir'</span> is purge";
+
+\Devrun\Utils\FileTrait::purge($dir);
 echo "<span style='color: darkmagenta; font-weight: bold;'>'$dir'</span> is purged";
